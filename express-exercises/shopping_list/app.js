4c6e67ecdd49deee32c31013204044264d2e63b4
Tiffany Lam <tlam5@sbcglobal.net>
2017-04-14 13:59:19 -0700

    Render shopping list without form

---
+++ express-exercises/shopping_list/app.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+
+var list = ["apples", "milk", "bread"];
+
+app.set('view engine', 'pug');
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function(req, res){
+  res.render('index', {list});
+});
+
+app.get('/search', function(req, res){
+  res.send(req.query);
+});
+
+app.get('/new-item', function(req, res) {
+
+});
+
+app.post('/add-item', function() {
+
+});
+
+app.listen(3000, function() {
+  console.log('Server is running on port 3000.');
+});
