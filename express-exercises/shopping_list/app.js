1ccdbde382c198ae38af5ae8e840fccd8455fed9
Tiffany Lam <tlam5@sbcglobal.net>
2017-04-17 14:20:59 -0700

    Include search function, new-item page, and ability to append to list

--- express-exercises/shopping_list/app.js
+++ express-exercises/shopping_list/app.js
@@ -1,25 +1,28 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
 
 var list = ["apples", "milk", "bread"];
 
 app.set('view engine', 'pug');
 app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.urlencoded({extended:true}));
 
 app.get('/', function(req, res){
   res.render('search', {list});
 });
 
 app.get('/search', function(req, res){
-  res.send(req.query);
+  res.send(req.query['keyword']);
 });
 
 app.get('/new-item', function(req, res) {
-
+  res.render('new-item');
 });
 
-app.post('/add-item', function() {
-
+app.post('/add-item', function(req, res) {
+  list.push(req.body['added-item']);
+  res.redirect('/');
 });
 
 app.listen(3000, function() {
