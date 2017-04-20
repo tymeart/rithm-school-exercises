70f4af61b8f283b1641950cbc60b5bb765140568
Tiffany Lam <tlam5@sbcglobal.net>
2017-04-20 16:29:14 -0700

    Initialize shopping list item routes

---
+++ express-middleware-routing/shopping_list/app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const methodOverride = require('method-override');
+const itemRoutes = require('./routes/items');
+
+app.set('view engine', 'pug');
+app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.urlencoded(extended: true));
+app.use(methodOverride('_method'));
+app.use('/items', itemRoutes);
+
+app.get('/', function(req, res, next) {
+  res.redirect('/items');
+});
+
+app.listen(3000, function() {
+  console.log('Server is listening on port 3000');
+});
---
+++ express-middleware-routing/shopping_list/routes/items.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const router = express.Router();
+
+var items = [];
+var id = 1;
+
+router.get('/', function(req, res, next) {
+
+});
+
+router.get('/new', function(req, res, next) {
+
+});
+
+router.get('/:id', function(req, res, next) {
+
+});
+
+router.get('/:id/edit', function(req, res, next) {
+
+});
+
+router.post('/', function(req, res, next) {
+
+});
+
+router.patch('/:id/edit', function(req, res, next) {
+
+});
+
+router.delete('/:id', function(req, res, next) {
+
+});
+
+module.exports = router;
