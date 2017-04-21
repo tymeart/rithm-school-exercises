82b89f20bb5ca436911afd494a021585c4c79df1
Tiffany Lam <tlam5@sbcglobal.net>
2017-04-21 14:59:59 -0700

    Render individual show page for any item passed into URL

--- express-middleware-routing/shopping_list/routes/items.js
+++ express-middleware-routing/shopping_list/routes/items.js
@@ -1,35 +1,36 @@
 const express = require('express');
 const router = express.Router();
 
-var items = [];
+var items = ['apples', 'grapes', 'tissue'];
 var id = 1;
 
 router.get('/', function(req, res, next) {
-
+  res.render('index', {items});
 });
 
 router.get('/new', function(req, res, next) {
-
+  res.render('new');
 });
 
 router.get('/:id', function(req, res, next) {
-
+  res.send(req.params.id);
 });
 
 router.get('/:id/edit', function(req, res, next) {
-
+  var item = items.find(val => val.id === Number(req.params.id));
+  res.render('edit', {item});
 });
 
-router.post('/', function(req, res, next) {
-
-});
-
-router.patch('/:id/edit', function(req, res, next) {
-
-});
-
-router.delete('/:id', function(req, res, next) {
-
-});
+// router.post('/', function(req, res, next) {
+//
+// });
+//
+// router.patch('/:id/edit', function(req, res, next) {
+//
+// });
+//
+// router.delete('/:id', function(req, res, next) {
+//
+// });
 
 module.exports = router;
